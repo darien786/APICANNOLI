1f9816c415ce3377c1568198bb07f3124e8cf4f6
Santiago840 <santyemma32@gmail.com>
2024-04-17 21:56:39 -0600

    Corrección exportar bd

--- dev/cannoli_db.sql
+++ dev/cannoli_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `cannoli_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cannoli_db`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: localhost    Database: cannoli_db
@@ -630,4 +632,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-17 21:24:36
+-- Dump completed on 2024-04-17 21:56:19
