07c954c90bc7d5b1496a7ba2308ac419ba1563c0
Santiago840 <santyemma32@gmail.com>
2024-04-29 01:48:22 -0600

    Merge branch 'main' into santiago

--- src/java/modelo/pojo/Categoria.java
+++ src/java/modelo/pojo/Categoria.java
@@ -15,16 +15,18 @@ public class Categoria {
     private String fotografia;
     private Integer estatus;
     private String nombreEstatus;
-    
+    private String fotografiaBase64;
+
     public Categoria() {
     }
-
-    public Categoria(Integer idCategoria, String nombreCategoria, String fotografia, Integer estatus, String nombreEstatus) {
+    
+    public Categoria(Integer idCategoria, String nombreCategoria, String fotografia, Integer estatus, String nombreEstatus, String fotografiaBase64) {
         this.idCategoria = idCategoria;
         this.nombreCategoria = nombreCategoria;
         this.fotografia = fotografia;
         this.estatus = estatus;
         this.nombreEstatus = nombreEstatus;
+        this.fotografiaBase64 = fotografiaBase64;
     }
 
     public Integer getIdCategoria() {
@@ -47,7 +49,7 @@ public class Categoria {
         return fotografia;
     }
 
-    public void setFotografia(String descripcion) {
+    public void setFotografia(String fotografia) {
         this.fotografia = fotografia;
     }
 
@@ -57,14 +59,22 @@ public class Categoria {
 
     public void setEstatus(Integer estatus) {
         this.estatus = estatus;
-    }    
-  
+    }
+
     public String getNombreEstatus() {
         return nombreEstatus;
     }
 
     public void setNombreEstatus(String nombreEstatus) {
         this.nombreEstatus = nombreEstatus;
+    }
+
+    public String getFotografiaBase64() {
+        return fotografiaBase64;
+    }
+
+    public void setFotografiaBase64(String fotografiaBase64) {
+        this.fotografiaBase64 = fotografiaBase64;
     }
     
 }
